03d73d57c994c349bcd9ffe708e4ae410924eb5e
VAWilliams <vicariowilliams@gmail.com>
2018-06-14 09:01:08 +0200

    Followed up on Homepage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 function roll() {
-    var burgerIcon=document.getElementById('burgerIcon');
-    console.log(burgerIcon);
-    
-    burgerIcon.style.animation = "roll 1.5s ease-in-out";    
+    var burgerIcon = document.getElementById('burgerIcon');
+    var burger = document.getElementById('burgerIcon');
+    burgerIcon.style.animation = "roll 1.5s ease-in-out";
+    lo
+}
+
+function again() {
+    var burger = document.getElementsByClassName('burger');
+    burgerIcon.style.animation = "roll 1.5s ease-in-out";  
 }
